e259e59589144dee7bd36b2c9c2436beaf7aace3
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2017-06-02 13:54:24 +0200

    Add read/write high-level methods; clear_feature control request.

--- lib/LibUSB/USBTMC.pm
+++ lib/LibUSB/USBTMC.pm
@@ -5,18 +5,22 @@ package LibUSB::USBTMC;
 use LibUSB;
 use Moose;
 use MooseX::Params::Validate 'validated_list';
-
+use Carp;
 
 use constant {
-    MsgID_DEV_DEP_MSG_OUT => 1,
-    MsgID_REQUEST_DEV_DEP_MSG_IN => 2,
-    MsgID_DEV_DEP_MSG_IN => 2,
-    MsgID_VENDOR_SPECIFIC_OUT => 126,
-    MsgID_REQUEST_VENDOR_SPECIFIC_IN => 127,
-    MsgID_VENDOR_SPECIFIC_IN => 127,
+    MSGID_DEV_DEP_MSG_OUT => 1,
+    MSGID_REQUEST_DEV_DEP_MSG_IN => 2,
+    MSGID_DEV_DEP_MSG_IN => 2,
+    MSGID_VENDOR_SPECIFIC_OUT => 126,
+    MSGID_REQUEST_VENDOR_SPECIFIC_IN => 127,
+    MSGID_VENDOR_SPECIFIC_IN => 127,
 
     MESSAGE_FINALIZES_TRANSFER => "\x{01}",
     MESSAGE_DOES_NOT_FINALIZE_TRANSFER => "\x{00}",
+
+    FEATURE_SELECTOR_ENDPOINT_HALT => 0,
+
+    BULK_HEADER_LENGTH => 12,
 };
 
 my $null_byte = "\x{00}";
@@ -77,22 +81,42 @@ has 'btag' => (
     default => 0,
     );
 
+has 'debug_mode' => (
+    is => 'ro',
+    isa => 'Bool',
+    default => 0
+    );
+
+has 'libusb_log_level' => (
+    is => 'ro',
+    isa => 'Int',
+    default => LIBUSB_LOG_LEVEL_WARNING,
+    );
+
+sub _debug {
+    my $self = shift;
+    if ($self->debug_mode()) {
+        carp @_;
+    }
+}
 
 sub BUILD {
     my $self = shift;
     my $ctx = LibUSB->init();
-    $ctx->set_debug(LIBUSB_LOG_LEVEL_WARNING);
+    $ctx->set_debug($self->libusb_log_level());
     
     my $handle = $ctx->open_device_with_vid_pid($self->vid(), $self->pid());
     my $device = $handle->get_device();
     
-
     eval {
+        # This will throw on windows and darwin. Catch exception with eval.
+        $self->_debug("enable auto detatch of kernel driver.");
         $handle->set_auto_detach_kernel_driver(1);
     };
 
     # FIXME: is interface always 0. Search for USBTMC interface?
 
+    $self->_debug("Claim USBTMC interface.");
     $handle->claim_interface(0);
     
     $self->_ctx($ctx);
@@ -100,6 +124,13 @@ sub BUILD {
     $self->_handle($handle);
 
     $self->_get_endpoint_addresses();
+
+    $self->_debug(
+        "Request clear_feature endpoint_halt for both bulk endpoints."
+        );
+    $self->clear_feature_endpoint_out();
+    $self->clear_feature_endpoint_in();
+
 }
 
 sub _get_endpoint_addresses {
@@ -107,6 +138,22 @@ sub _get_endpoint_addresses {
     # FIXME: loop over endpoints. This is just for Agilent 34410A.
     $self->_bulk_out_endpoint(0x2);
     $self->_bulk_in_endpoint(0x86);
+}
+
+sub write {
+    my $self = shift;
+    return $self->dev_dep_msg_out(@_);
+}
+
+sub read {
+    my $self = shift;
+    my ($length, $timeout) = validated_list(
+        \@_,
+        length => {isa => 'Int'},
+        timeout => {isa => 'Int', default => 5000}
+        );
+    $self->request_dev_dep_msg_in(length => $length, timeout => $timeout);
+    return $self->dev_dep_msg_in(length => $length, timeout => $timeout);
 }
 
 sub dev_dep_msg_out {
@@ -117,12 +164,38 @@ sub dev_dep_msg_out {
         timeout => {isa => 'Int', default => 5000},
         );
     
+    $self->_debug("doing dev_dep_msg_out with data $data");
+    
     my $header = $self->_dev_dep_msg_out_header(length => length $data);
     my $endpoint = $self->bulk_out_endpoint();
 
     # Ensure that total number of bytes is multiple of 4.
-    $data .= $null_byte x ((length $data) % 4); 
+    $data .= $null_byte x ((4 - (length $data) % 4) % 4);
     $self->handle()->bulk_transfer_write($endpoint, $header . $data, $timeout);
+}
+
+sub dev_dep_msg_in {
+    my $self = shift;
+    my ($length, $timeout) = validated_list(
+        \@_,
+        length => {isa => 'Int'},
+        timeout => {isa => 'Int', default => 5000}
+        );
+    
+    $self->_debug("doing dev_dep_msg_in with length $length");
+    
+    my $endpoint = $self->bulk_in_endpoint();
+    my $data = $self->handle()->bulk_transfer_read(
+        $endpoint, $length + BULK_HEADER_LENGTH, $timeout
+        );
+    
+    if (length $data < BULK_HEADER_LENGTH) {
+        croak "dev_dep_msg_in does not contain header";
+    }
+    
+    my $header = substr($data, 0, BULK_HEADER_LENGTH);
+    $data = substr($data, BULK_HEADER_LENGTH);
+    return $data;
 }
 
 sub request_dev_dep_msg_in {
@@ -132,8 +205,8 @@ sub request_dev_dep_msg_in {
         length => {isa => 'Int', default => 1000},
         timeout => {isa => 'Int', default => 5000},
         );
-    
-    my $header = $self->_dev_dep_msg_in_header(length => $length);
+    $self->_debug("doing request_dev_dep_msg_in with length $length");
+    my $header = $self->_request_dev_dep_msg_in_header(length => $length);
     my $endpoint = $self->bulk_out_endpoint();
 
     # Length of $header is already multiple of 4.
@@ -144,7 +217,7 @@ sub _dev_dep_msg_out_header {
     my $self = shift;
     my ($length) = validated_list(\@_, length => {isa => 'Int'});
     
-    my $header = $self->_bulk_out_header(MsgID => MsgID_DEV_DEP_MSG_OUT);
+    my $header = $self->_bulk_out_header(MSGID => MSGID_DEV_DEP_MSG_OUT);
     $header .= pack('V', $length);
     $header .= MESSAGE_FINALIZES_TRANSFER;
     $header .= $null_byte x 3;  # Reserved bytes.
@@ -154,19 +227,22 @@ sub _dev_dep_msg_out_header {
 sub _request_dev_dep_msg_in_header {
     my $self = shift;
     my ($length) = validated_list(\@_, length => {isa => 'Int'});
-    my $header = $self->_bulk_out_header(MsgID => MsgID_REQUEST_DEV_DEP_MSG_IN);
+    my $header = $self->_bulk_out_header(MSGID => MSGID_REQUEST_DEV_DEP_MSG_IN);
+    # Transfer length
     $header .= pack('V', $length);
+    # Term char enabled? 2 or 0. make argument
     $header .= pack('C', 2); # Fixme: make argument
-    $header .= '\n';         # Term char
+    $header .= "\n";         # Term char
     $header .= $null_byte x 2; # Reserved. Must be 0x00.
+    
     return $header;
 }
 
 
 sub _bulk_out_header {
     my $self = shift;
-    my ($MsgID) = validated_list(\@_, MsgID => {isa => 'Int'});
-    my $bulk_out_header = pack('C', $MsgID);
+    my ($MSGID) = validated_list(\@_, MSGID => {isa => 'Int'});
+    my $bulk_out_header = pack('C', $MSGID);
     my ($btag, $btag_inverse) = $self->_btags();
     $bulk_out_header .= $btag . $btag_inverse;
 
@@ -198,6 +274,36 @@ sub clear {
     my $wIndex = 0; # FIXME: interface number
     return $self->handle()->control_transfer_read($bmRequestType, $bRequest, $wValue, $wIndex, 1, $timeout);
     
+}
+
+sub clear_feature_endpoint_out {
+    my $self = shift;
+    my ($timeout) = validated_list(
+        \@_, timeout => {isa => 'Int', default => 5000});
+    
+    my $endpoint = $self->bulk_out_endpoint();
+    my $bmRequestType = LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_STANDARD
+        | LIBUSB_RECIPIENT_ENDPOINT;
+    my $bRequest = LIBUSB_REQUEST_CLEAR_FEATURE;
+    my $wValue = FEATURE_SELECTOR_ENDPOINT_HALT;
+    my $wIndex = $endpoint;
+    $self->handle()->control_transfer_write(
+        $bmRequestType, $bRequest, $wValue, $wIndex, "", $timeout);
+}
+
+sub clear_feature_endpoint_in {
+    my $self = shift;
+    my ($timeout) = validated_list(
+        \@_, timeout => {isa => 'Int', default => 5000});
+    
+    my $endpoint = $self->bulk_in_endpoint();
+    my $bmRequestType = LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_STANDARD
+        | LIBUSB_RECIPIENT_ENDPOINT;
+    my $bRequest = LIBUSB_REQUEST_CLEAR_FEATURE;
+    my $wValue = FEATURE_SELECTOR_ENDPOINT_HALT;
+    my $wIndex = $endpoint;
+    $self->handle()->control_transfer_write(
+        $bmRequestType, $bRequest, $wValue, $wIndex, "", $timeout);
 }
 
 sub clear_halt_out {
--- scripts/test.pl
+++ scripts/test.pl
@@ -3,14 +3,19 @@ use 5.020;
 use warnings;
 use strict;
 use lib 'lib';
+use LibUSB;
 use LibUSB::USBTMC;
 
-my $driver = LibUSB::USBTMC->new(vid => 0x0957, pid => 0x0607);
+my $driver = LibUSB::USBTMC->new(
+    vid => 0x0957, pid => 0x0607,
+    debug_mode => 0, 
+    # libusb_log_level => LIBUSB_LOG_LEVEL_DEBUG
+    );
 
-
-$driver->clear_halt_in();
-$driver->clear_halt_out();
-
-$driver->dev_dep_msg_out(data => "*RST\n");
-$driver->dev_dep_msg_out(data => "VOLT:RANGE 10\n");
-
+$driver->write(data => "*CLS\n");
+$driver->write(data => "*RST\n");
+for (1..1000) {
+    $driver->write(data => "*RST\n");
+    # $driver->write(data => "*IDN?\n");
+    # print "idn: ", $driver->read(length => 200);
+}
